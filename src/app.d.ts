e1d363059aaed577a561fde4ba51f0a4f30d9c2e
VDawg <vdawg@tuta.io>
2024-09-25 19:59:03 +0300

    commit save point

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,9 @@
+import 'unplugin-icons/types/svelte'
+
+/// <reference types="svelte" />
+/// <reference types="vite/client" />
+/// <reference types="unplugin-icons/types/svelte" />
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
